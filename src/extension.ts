4c5031563674634ea39a40ed55737fa56e7648d0
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2024-06-07 11:01:46 -0500

    Add draft version of typescript-based completion support

---
+++ src/extension.ts
@@ -0,0 +1,25 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+import * as vscode from "vscode";
+import { nativeFunctionsCompletions } from "./nativeFunctions";
+
+export function activate(context: vscode.ExtensionContext) {
+  const provider1 = vscode.languages.registerCompletionItemProvider("bell", {
+    provideCompletionItems(
+      document: vscode.TextDocument,
+      position: vscode.Position,
+      token: vscode.CancellationToken,
+      context: vscode.CompletionContext
+    ) {
+      const forLoopCompletion = new vscode.CompletionItem("For loop");
+      forLoopCompletion.insertText = new vscode.SnippetString("for $${1|i|} in $${2} ${3|collect,do|} (${4});");
+
+      // return all completion items as array
+      return [...nativeFunctionsCompletions, forLoopCompletion];
+    },
+  });
+
+  context.subscriptions.push(provider1);
+}
