2d57797d07189461cd70105d5cbe5693e2ea2a0e
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2024-06-13 16:14:03 -0500

    minor edit

--- src/extension.ts
+++ src/extension.ts
@@ -117,7 +117,7 @@ export function activate(context: vscode.ExtensionContext) {
       const end = new vscode.Position(document.lineCount, document.lineAt(document.lineCount - 1).range.end.character);
       const range = new vscode.Range(start, end);
       const replace = replaceTree(tree);
-      return [vscode.TextEdit.replace(range, replace)];
+      return [vscode.TextEdit.replace(range, JSON.stringify(tree, null, 2))];
     },
   });
 
