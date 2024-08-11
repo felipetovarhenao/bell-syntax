4ced35f2d241436cb8ca973df98f03d46f85d474
Felipe Tovar-Henao <pipetovarh@hotmail.com>
2024-08-11 17:09:13 -0500

    Add indentation to formatter draft

--- src/formatter.ts
+++ src/formatter.ts
@@ -1,13 +1,14 @@
 import { SemanticNode } from "./parser";
-export default function format(nodes: SemanticNode[]) {
+export default function format(nodes: SemanticNode[], depth: number = 0) {
   let code: string = "";
   for (let i = 0; i < nodes.length; i += 1) {
     const node = nodes[i];
-    code += "\n";
+    const linebreak = "\n" + " ".repeat(depth * 4);
+    code += linebreak;
     if (node.token.nestable && node.children) {
       code += node.token.begins;
-      code += format(node.children);
-      code += "\n";
+      code += format(node.children, depth + 1);
+      code += linebreak;
       code += node.token.ends;
     } else {
       code += node.substring + ` ## ${node.token.type.toLowerCase()}`;
